bf7a7630ecdb36063388f49ae8c128a487dee87e
LuK1337 <priv.luk@gmail.com>
2024-04-06 16:05:39 +0200

    soong: Add `aapt_version_code` default
    This appends `--version_code=$(date -u +%Y%m%d)` to aapt flags, which is
    useful for flushing some caches upon system updates.
    
    Change-Id: I6575b878f09c1c3138e12abc34d39405f51245e7
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/11789
    Reviewed-by: Sourajit Karmakar <sourajitk@statixos.com>
    Tested-by: Sourajit Karmakar <sourajitk@statixos.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -28,6 +28,7 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 
 SOONG_CONFIG_NAMESPACES += hentaiGlobalVars
 SOONG_CONFIG_hentaiGlobalVars += \
+    aapt_version_code \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
     target_init_vendor_lib \
@@ -55,6 +56,7 @@ TARGET_INIT_VENDOR_LIB ?= vendor_init
 TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
+SOONG_CONFIG_hentaiGlobalVars_aapt_version_code := $(shell date -u +%Y%m%d)
 SOONG_CONFIG_hentaiGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_hentaiGlobalVars_bootloader_message_offset := $(BOOTLOADER_MESSAGE_OFFSET)
 SOONG_CONFIG_hentaiGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
